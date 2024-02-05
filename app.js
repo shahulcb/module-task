393087e725df40b9f1e31858275b8f43f0a7b522
shahul cb <theshahulofcl@gmail.com>
2024-02-05 21:53:32 +0530

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({ credentials: true, origin: true }))
 
 
 module.exports = app
